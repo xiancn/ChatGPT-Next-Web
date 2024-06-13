9fe0d2e4c9307dda1f1e99f6510f7fef69f2e17e
Brad J Herring <53147565+xiancn@users.noreply.github.com>
2024-06-13 15:13:12 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -15,7 +15,7 @@ export const ANTHROPIC_BASE_URL = "https://api.anthropic.com";
 export const GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/";
 
 export enum Path {
-  Home = "/auth",
+  Home = "/",
   Chat = "/chat",
   Settings = "/settings",
   NewChat = "/new-chat",
